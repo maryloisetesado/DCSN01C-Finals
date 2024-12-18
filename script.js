3ef069f45291ce6d7ec0ab422ad321fc39ab01dc
maryloisetesado <mldt.social@gmail.com>
2024-12-18 21:23:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function validateForm() {
+  let x = document.forms["myForm"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["email"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+function validateForm() {
+  let x = document.forms["myForm"]["message"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
